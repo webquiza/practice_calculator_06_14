d731c1309a2bc2c0fa8243c40f9c78ec7326f0d7
Carlos Urquiza <webquiza@gmail.com>
2021-06-14 16:07:35 -0700

    add subtract logic

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,22 @@ function divide(number1, number2) {
 
 // User interface logic 
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $("#output1").text(result);
   });
 });
 
+$(document).ready(function() {
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#sub1").val());
+    let number2 = parseInt($("#sub2").val());
+    let result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+});
