7809f2685d19582617aef30dbfb93e26065d44e9
Carlos Urquiza <webquiza@gmail.com>
2021-06-14 15:11:08 -0700

    add calculator user and business logic

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Business logic:
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// User interface logic 
+
+$(document).ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
