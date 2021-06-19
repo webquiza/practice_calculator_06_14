8d8745fe7d52f327ec1278c8610b3ef9a5160765
Carlos Urquiza <webquiza@gmail.com>
2021-06-18 18:04:24 -0700

    join all operators into one form

--- js/scripts.js
+++ js/scripts.js
@@ -19,21 +19,21 @@ function divide(number1, number2) {
 // User interface logic 
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output1").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    let number1 = parseInt($("#sub1").val());
-    let number2 = parseInt($("#sub2").val());
-    let result = subtract(number1, number2);
-    $("#output2").text(result);
+  $("form#calculator").submit(function(e) {
+    e.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
 });
